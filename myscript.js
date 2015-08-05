bc0c7064f5652f12e5be6d1890b2b68d6521c489
Anoop Hallur <hallur@adobe.com>
2015-08-04 23:34:53 -0700

    works

--- myscript.js
+++ myscript.js
@@ -1,4 +1,10 @@
-var url = location.href;
-if(url.indexOf("?share=true") < 0){
-	location.href = url + "?share=true";	
-}
+// Get rid of the signup popup box
+var dialog_wrappers = document.getElementsByClassName("dialog_wrapper");
+var dialog_wrapper = dialog_wrappers[0];
+dialog_wrapper.style.display='none';
+
+//  Take care of the background colour. i.e make it white again
+var modal_signup_dialogs = document.getElementsByClassName("modal_signup_dialog");
+var modal_signup_dialog = modal_signup_dialogs[0];
+modal_signup_dialog.previousSibling.style.display='none';
+modal_signup_dialog.style.display='none';
